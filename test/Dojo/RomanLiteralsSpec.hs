20f71ba5b2acf624c8bdfb309eb3a6f7b3c43a6c
Mahler, Thomas <thomas.mahler@ista.com>
2020-04-20 09:54:48 +0200

    factor out csv test function

--- test/Dojo/RomanLiteralsSpec.hs
+++ test/Dojo/RomanLiteralsSpec.hs
@@ -6,13 +6,14 @@ import           Test.QuickCheck
 
 import           RomanLiterals
 
+csvTest :: [(String,Integer)] -> IO ()
+csvTest = mapM_ (\ (str, val) -> convert str `shouldBe` val)
+
 spec :: Spec
 spec =
   describe "RomanLiterals" $ do
     it "parses single literals" $
-     mapM_
-        (\(str, val) -> convert str `shouldBe` val)
-        [("i",1),("v",5),("x", 10),("l",50),("c",100),("d",500),("m",1000)]
+     csvTest [("i",1),("v",5),("x", 10),("l",50),("c",100),("d",500),("m",1000)]
     it "parses cc" $
      convert "cc" `shouldBe` 200
     it "parses lx" $
@@ -27,5 +28,3 @@ spec =
       convert "MDCCCCLXXXIIII" `shouldBe` 1984
     it "parses MCMLXXXIV" $
       convert "MCMLXXXIV" `shouldBe` 1984
-
-
